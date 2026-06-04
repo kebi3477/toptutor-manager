3e4adc30838bd4b68a3d1ffd2c67f34f8a2848a4
kebi_ko <kebi3477@naver.com>
2026-06-04 16:02:58 +0900

    deploy: 포트 3001 -> 3000 변경

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('api/health')
+  @Get('health')
   health(): { status: string } {
     return { status: 'ok' };
   }
